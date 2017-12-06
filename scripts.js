cd006b7257ec2fd1953a8cd2af02e51123e716cc
Patrick McLaughlin <patrickmc21@gmail.com>
2017-12-06 13:37:17 -0700

    .

--- scripts.js
+++ scripts.js
@@ -14,9 +14,12 @@ var randomNum = 0;
 var clickRandom = document.getElementById('generate-random');
 var form = document.querySelector('form');
 var levelUp = document.getElementById('level-start');
+var levelCounter = 1
+var currentLevel = 'Level ' + levelCounter;
 
 // functions run on page load
 
+console.log(currentLevel);
 storeRandom ();
 disableButton();
 
@@ -56,6 +59,9 @@ function storeRandom () {
 
 function reset () {
     interactiveZone.style.visibility = 'hidden';
+    levelCounter = 1
+    currentLevel = 'level ' + levelCounter;
+    console.log(currentLevel);
     max = 100
     min = 1
     backgroundRemove ('cold');
@@ -64,7 +70,7 @@ function reset () {
     error.innerText = '';
     levelUp.style.visibility = 'hidden';
     guessDeclaration.visibility = 'hidden';
-    clickGuess.addEventListener('click', contextCorrect);
+    // clickGuess.addEventListener('click', contextCorrect);
     disableButton();
 }
 
@@ -90,16 +96,22 @@ function logic (){
             var guessedNumber = parseInt(userInput.value);          
             if (guessedNumber == randomNum) {
                 userInput.value = '';
+                levelCounter = levelCounter + 1
+                currentLevel = 'level ' + levelCounter;
                 guessDeclaration.innerText = 'BOOM!';
                 backgroundRemove ('cold');
                 backgroundAdd ('winner-guess');
                 levelUp.style.visibility = 'visible';
-                levelUp.innerText = 'Start Level 2';
+                levelUp.innerText = 'Proceed to ' + currentLevel;
                 levelUp.addEventListener('click', 
                     function(event){
                         event.preventDefault()
                     });    
-                levelUp.addEventListener('click', levelTwo);
+                if (levelCounter == 2) {
+                    levelUp.addEventListener('click', levelTwo);
+                }else if (levelCounter == 3) {
+                     levelUp.addEventListener('click', levelThree);
+                }
             }else if (guessedNumber > randomNum) {
                 guessDeclaration.innerText = 'Your guess is too high';
                 backgroundAdd ('cold');
@@ -146,78 +158,18 @@ function contextCorrect (){
 
 // Level two start
 
-function contextCorrectTwo (){
-    var el = (isNaN(parseInt(userInput.value)));
-    var error = document.getElementById('error')
-    var visible = error.style.visibility = 'visible'
-    var guessedNumber = parseInt(userInput.value);
-    if (userInput.value  == '') {
-        visible;
-        error.innerText = 'Please insert a number between ' + min + ' and ' + max;
-    }else if (el) {   
-        visible;
-        error.innerText = userInput.value + ' is not a number';
-        activeButton();
-        console.log('true not a num');
-    }else if (!guessedNumber % 1 !== 0) {
-        console.log(guessedNumber % 1);
-        activeButton ();
-        visible;
-        error.innerText = userInput.value + ' is not a whole number';   
-    }else if (((guessedNumber < min) || (guessedNumber > max)) == true) {
-        activeButton();
-        visible;
-        error.innerText = 'Pick a number between ' 
-        + min + ' and ' + max;    
-    }else { 
-        activeButton();
-        error.style.visibility = 'hidden';
-        console.log('proceed'); 
-        activate();
-        logicTwo();           
-    }
-};
-
-function logicTwo (){
-            var guessedNumber = parseInt(userInput.value);          
-            if (guessedNumber == randomNum) {
-                guessDeclaration.innerText = 'BOOM!';
-                backgroundRemove ('cold');
-                backgroundAdd ('winner-guess');
-                levelUp.style.visibility = 'visible';
-                levelUp.innerText = 'Start Level 3';
-            }else if (guessedNumber > randomNum) {
-                guessDeclaration.innerText = 'Your guess is too high';
-                backgroundAdd ('cold');
-            }else if (guessedNumber < randomNum) {
-                guessDeclaration.innerText = 'Your guess is too low';
-                backgroundAdd ('cold');
-            }else {
-                console.log('fail');
-            }
-            console.log(randomNum + ' random');
-            console.log(guessedNumber + ' user input');
-        };
-
 function levelTwo (){
+    console.log(currentLevel);
     max = 200
     min = 101
     randomNum = 0;
     console.log(randomNum + ' one');
     storeRandom();
     console.log(randomNum + ' two');
-    // function(event){
-    //     event.preventDefault();
-    // };
-    // randomNum = 0;
-    clickGuess.addEventListener('click', contextCorrectTwo);
     clear ();
     interactiveZone.style.visibility = 'hidden';
     backgroundRemove ('winner-guess');
     levelUp.style.visibility = 'hidden';
-    // storeRandom(200, 100);
-    // console.log(randomNum);
-    contextCorrectTwo ();
 };
 
 
